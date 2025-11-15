8d157206227033c074e498cbae54a2f83fa989af
panpan <panpan31415@gmail.com>
2025-11-15 12:15:06 +0100

    a user button is added

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -15,10 +15,7 @@ const Header = () => {
               height={48}
               priority={true}
             />
-            <span
-              className="hidden lg:block font-bold text-2xl ml-3 
-"
-            >
+            <span className="hidden lg:block font-bold text-2xl ml-3">
               {APP_NAME}
             </span>
           </Link>
