b3b78419a0bb8c0d9f8ce9bd705610de1fc35704
panpan <panpan31415@gmail.com>
2025-11-17 15:46:47 +0100

    add useTransition

--- components/shared/product/add-to-cart-button.tsx
+++ components/shared/product/add-to-cart-button.tsx
@@ -3,53 +3,62 @@
 import { Button } from "@/components/ui/button";
 import { addItem, removeCartItem } from "@/lib/actions/cart.actions";
 import { Cart, CartItem } from "@/types";
-import { Minus, Plus } from "lucide-react";
+import { Loader, Minus, Plus } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useTransition } from "react";
 import { toast } from "sonner";
 
 const AddToCardButton = ({ item, cart }: { item: CartItem; cart?: Cart }) => {
   const router = useRouter();
-
-  const removeItem = async () => {
-    const res = await removeCartItem(item.productId);
-    if (res.success) {
-      toast.success(res.message);
-      return;
-    }
-    toast.error(res.message);
+  const [increasing, startIncreasing] = useTransition();
+  const [decreasing, startDecreasing] = useTransition();
+  const [adding, startAdding] = useTransition();
+  const removeItem = () => {
+    startDecreasing(async () => {
+      const res = await removeCartItem(item.productId);
+      if (res.success) {
+        toast.success(res.message);
+        return;
+      }
+      toast.error(res.message);
+    });
   };
 
-  const addItemToCart = async () => {
-    const res = await addItem(item);
-    if (res.success) {
-      toast.success(res.message);
-      return;
-    }
-    toast.error(res.message);
+  const addItemToCart = () => {
+    startIncreasing(async () => {
+      const res = await addItem(item);
+      if (res.success) {
+        toast.success(res.message);
+        return;
+      }
+      toast.error(res.message);
+    });
   };
 
-  const addToCart = async () => {
-    const res = await addItem(item);
-    if (res.success) {
-      toast.success("Success", {
-        position: "bottom-right",
-        description: `${item.name} added to cart`,
-        action: (
-          <Button
-            className="bg-primary   hover:bg-gray-400 hover:cursor-pointer"
-            onClick={() => router.push("/cart")}
-          >
-            Go to cart
-          </Button>
-        ),
-      });
-    } else {
-      toast.error("Failed", {
-        position: "bottom-right",
-        description: res.message,
-      });
-      return;
-    }
+  const addToCart = () => {
+    startAdding(async () => {
+      const res = await addItem(item);
+      if (res.success) {
+        toast.success("Success", {
+          position: "bottom-right",
+          description: `${item.name} added to cart`,
+          action: (
+            <Button
+              className="bg-primary   hover:bg-gray-400 hover:cursor-pointer"
+              onClick={() => router.push("/cart")}
+            >
+              Go to cart
+            </Button>
+          ),
+        });
+      } else {
+        toast.error("Failed", {
+          position: "bottom-right",
+          description: res.message,
+        });
+        return;
+      }
+    });
   };
 
   // check if item is in cart
@@ -59,11 +68,19 @@ const AddToCardButton = ({ item, cart }: { item: CartItem; cart?: Cart }) => {
   return existingItem ? (
     <div className="w-full hover:cursor-pointer flex justify-around items-center">
       <Button type="button" variant={"outline"} onClick={removeItem}>
-        <Minus className="h-4 w-4" />
+        {decreasing ? (
+          <Loader className="h-4 w-4 animate-spin" />
+        ) : (
+          <Minus className="h-4 w-4" />
+        )}
       </Button>
       <span className="x-2">{existingItem?.qty || 0}</span>
       <Button type="button" variant={"outline"} onClick={addItemToCart}>
-        <Plus className="h-4 w-4" />
+        {increasing ? (
+          <Loader className="h-4 w-4 animate-spin" />
+        ) : (
+          <Plus className="h-4 w-4" />
+        )}
       </Button>
     </div>
   ) : (
@@ -72,7 +89,7 @@ const AddToCardButton = ({ item, cart }: { item: CartItem; cart?: Cart }) => {
       type="button"
       onClick={addToCart}
     >
-      <Plus />
+      {adding ? <Loader className="h-4 w-4 animate-spin" /> : <Plus />}
       Add To Cart
     </Button>
   );
